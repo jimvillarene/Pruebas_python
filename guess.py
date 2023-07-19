a1a8ddcc500ed5a9bab623a25630a04666d19d6a
jimvillarene <jimvillarene@gmail.com>
2023-07-19 14:34:10 -0600

    Update guess.py

--- guess.py
+++ guess.py
@@ -2,19 +2,23 @@
 import random
 print("Hola, cuál es tu nombre")
 nombre=input()
-print("Hola, "+nombre+". Estoy pensando en un numero entre el 1 y el 10, ¿Puedes adivinar cál es")
-numero=input()
-magicNum=random.randint(0,11)
-while numero != magicNum
-	if numero < magicNum:
-		print("Tu numero está demasiado bajo. Inténtalo otra vez)
-		numero=input()
-		continue
-	elif numero > magicNum:
-		print("Tu número está demasiado alto. Inténtalo otra vez")
-		numero=input()
-	else:
-		break
-print("Felicidades, diste en el blanco")
-		
-
+print("Hola, "+nombre+". Estoy pensando en un numero entre el 1 y el 10, ¿Puedes adivinar cuál es")
+numero=0
+magicNum=random.randint(1,11)
+intentos=1
+while numero != magicNum and intentos < 7:
+    print("Intenta adivinar")
+    numero=int(input())
+    if numero < magicNum:
+        print("Tu numero está demasiado bajo. Inténtalo otra vez")
+        intentos+=1
+        continue
+    elif numero > magicNum:
+        print("Tu número está demasiado alto. Inténtalo otra vez")
+        intentos+=1
+    else:
+        break
+if numero == magicNum:
+    print("Felicidades, diste en el blanco después de "+str(intentos)+" intentos")
+else:
+	print("Lo siento no diste en el blanco. El número era: "+str(magicNum))		
